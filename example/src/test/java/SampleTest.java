cbb2df9ea494374677f0124d67bee5fab271c4a5
shunsuke maeda <duck8823@gmail.com>
2018-05-27 21:14:13 +0900

    Add example

---
+++ example/src/test/java/SampleTest.java
@@ -0,0 +1,22 @@
+import com.github.duck8823.GenerateNames;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@GenerateNames(findMethods = true)
+class SampleTest {
+
+	@ParameterizedTest
+	@MethodSource(SampleTestNames.NAMES)
+	void test(String arg) {
+		assertEquals(arg, "a");
+	}
+
+	static Stream<String> names() {
+		return Stream.of("a", "b");
+	}
+}
